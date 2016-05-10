73cdb8865d70f47375feb83cbdeec6dff4efc084
Jerry Bi <jerrybi0522@gmail.com>
2016-05-10 19:42:09 -0400

    attempting to add validations

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,4 +1,6 @@
 class Boat < ActiveRecord::Base
 	belongs_to :user
 	has_many :jobs
+	validates :name, uniqueness: true
+	validates :location, inclusion: {in: ["NYC", "LA", "Miami"]} #, "Chicago", "Boston", "New Orleans", "Detroit", "Seattle"]
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,9 @@
 class Job < ActiveRecord::Base
 	belongs_to :boat
 	belongs_to :user, through: :boat
+	validates :name, uniqueness: :true
+	validates :origin, inclusion: {in: ["Paris", "Tokyo", "London"]}
+	validates_numericality_of :cost, :greater_than => 1000.00
+	validates :cargo, length: [minimum: 50]
+	validates :destination, inclusion: {in: ["Hong Kong", "Moscow", "Berlin"]}
 end
