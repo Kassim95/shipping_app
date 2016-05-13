e1627ce55be4ab37f8507021293330db4bbbb987
Sam Berman <samuelaberman@gmail.com>
2016-05-13 14:09:30 -0400

    Fixing conflict

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,21 @@
+Boat.create([{name: "Wayfarer", containers: "3", location: "NYC"}])
+Boat.create([{name: "Honolulu Sunset", containers: "5", location: "Miami"}])
+Boat.create([{name: "Wayfarer", containers: "10", location: "LA"}])
+
+
+
+
+
+
+
+
+
+Job.create([{containers:"5", cargo:"", origin:"Boston", destination:"London", cost: "1100"}])
+Job.create([{containers:"10", cargo:"", origin:"", destination:"", cost: "5000"}])
+Job.create([{containers:"3", cargo:"", origin:"", destination:"", cost: "2000"}])
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
