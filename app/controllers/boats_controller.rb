7fc52fc5e7f8a670481e32780f30804cc2ae2d0b
Kassim Ouattara <ouat.kassim@gmail.com>
2016-05-16 17:03:13 -0400

    Adding the follow function

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -37,6 +37,9 @@ class BoatsController < ApplicationController
     @boat = Boat.find(params[:id])
     @job = Job.new
     @jobs = Job.where(boat_id: params[:id])
+   @follow = Follower.where(:user_id => current_user.id, :boat_id => @boat.id).first
+    puts @like
+
   end
 
   private
