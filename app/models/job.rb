fd47696d5f0c259c5326af9fe9e572eefb0a1534
Jerry Bi <jerrybi0522@gmail.com>
2016-05-20 12:23:49 -0400

    fixing validation error

--- app/models/job.rb
+++ app/models/job.rb
@@ -6,6 +6,6 @@ class Job < ActiveRecord::Base
 	validates :name, uniqueness: :true
 	validates :origin, inclusion: {in: ["Paris", "Tokyo", "London"]}
 	validates_numericality_of :cost, :greater_than => 1000.00
-	# validates :cargo, length: [minimum: 50]
+	validates :cargo, length: { minimum: 50 }
 	validates :destination, inclusion: {in: ["Hong Kong", "Moscow", "Berlin"]}
 end
